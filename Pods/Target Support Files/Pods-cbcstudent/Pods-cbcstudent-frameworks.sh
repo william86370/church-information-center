0113dd517637ebc34d20a2dd96a17e7cedd7ced8
William Wright <william86370@gmail.com>
2017-03-24 03:03:25 -0400

    removed useless pods i wasn't using

--- Pods/Target Support Files/Pods-cbcstudent/Pods-cbcstudent-frameworks.sh
+++ Pods/Target Support Files/Pods-cbcstudent/Pods-cbcstudent-frameworks.sh
@@ -84,7 +84,6 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/FeedKit/FeedKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JTMaterialSpinner/JTMaterialSpinner.framework"
@@ -92,7 +91,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Protobuf/Protobuf.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/FeedKit/FeedKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JTMaterialSpinner/JTMaterialSpinner.framework"
